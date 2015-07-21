fc77f2c70cb93b219b06cc765f24f4ef23de238c
Paul Dariye <paul.dariye@gmail.com>
2015-07-20 21:56:57 -0400

    Update screenings controller to ignore csrf verification if content type is json

--- app/controllers/api/v1/screenings_controller.rb
+++ app/controllers/api/v1/screenings_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ScreeningsController < ApplicationController
-  # skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token, :only => [:create, :update, :delete]
   before_filter :verified_request?
   respond_to :json
 
