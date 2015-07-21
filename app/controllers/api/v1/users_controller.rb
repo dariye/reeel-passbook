f80224e3dedbfd85a299455d3a72f318f7358f11
Paul Dariye <paul.dariye@gmail.com>
2015-07-20 21:16:21 -0400

    Add 'skip_before_action' in users controller for token

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   respond_to :json
-  
+  skip_before_filter :verify_authenticity_token  
   def index 
     respond_with User.search(params)
   end
