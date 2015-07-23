cd08b9f517e459b11adfbc12d886f5f30b795f72
Paul Dariye <paul.dariye@gmail.com>
2015-07-23 11:45:31 -0400

    update

--- app/controllers/api/v1/passes_controller.rb
+++ app/controllers/api/v1/passes_controller.rb
@@ -3,8 +3,7 @@ require "#{Rails.root}/lib/passbook_helpers/utils.rb"
 class Api::V1::PassesController < ApplicationController
   include PassbookHelpers::Utils
   respond_to :json
-  skip_before_action :verify_authenticity_token # <-- find another way
-  
+  # skip_before_action :verify_authenticity_token # <-- find another way
   def index
     respond_with Pass.search(params)
   end
